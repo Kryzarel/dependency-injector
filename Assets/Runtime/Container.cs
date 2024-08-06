7aa4f3659970a5d00b5342785870940a31cd3fad
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-08-07 00:47:04 +0100

    Changed how registrations and object instances are stored

--- Assets/Runtime/Container.cs
+++ Assets/Runtime/Container.cs
@@ -8,13 +8,11 @@ namespace Kryz.DI
 		private readonly struct Registration
 		{
 			public readonly Type Type;
-			public readonly object? Object;
 			public readonly bool Transient;
 
-			public Registration(Type type, object? obj, bool transient = false)
+			public Registration(Type type, bool transient = false)
 			{
 				Type = type;
-				Object = obj;
 				Transient = transient;
 			}
 		}
@@ -24,7 +22,8 @@ namespace Kryz.DI
 		public readonly IReadOnlyList<Container> Children;
 
 		private readonly List<Container> children = new();
-		private readonly Dictionary<Type, Registration> objects = new();
+		private readonly Dictionary<Type, object> objects = new();
+		private readonly Dictionary<Type, Registration> registrations = new();
 		private readonly ReflectionInjector reflectionInjector;
 
 		public Container()
@@ -66,22 +65,7 @@ namespace Kryz.DI
 
 		public bool TryGet(Type type, out object? obj)
 		{
-			Container? container = this;
-			while (container != null)
-			{
-				if (container.objects.TryGetValue(type, out Registration registration))
-				{
-					obj = registration.Object ?? CreateAndInject(registration.Type);
-					if (registration.Object == null && !registration.Transient)
-					{
-						container.objects[type] = new Registration(registration.Type, obj);
-					}
-					return true;
-				}
-				container = Parent;
-			}
-			obj = null;
-			return false;
+			return TryGet(this, type, out obj);
 		}
 
 		public T Get<T>()
@@ -105,36 +89,77 @@ namespace Kryz.DI
 			reflectionInjector.Inject(typeof(T), obj, this);
 		}
 
-		public Container AddSingleton<T>(T obj) => AddSingleton<T, T>(obj);
-		public Container AddScoped<T>(T obj) => AddScoped<T, T>(obj);
+		public Container AddSingleton<T>(T obj) where T : notnull => AddSingleton<T, T>(obj);
+		public Container AddScoped<T>(T obj) where T : notnull => AddScoped<T, T>(obj);
 		public Container AddTransient<T>() => AddTransient<T, T>();
 
-		public Container AddSingleton<TBase, TDerived>(bool lazy = false) where TDerived : TBase
+		public Container AddSingleton<TBase, TDerived>() where TDerived : TBase
 		{
-			return AddSingleton<TBase, TDerived>(lazy ? default : CreateAndInject<TDerived>());
+			return Root.AddScoped<TBase, TDerived>();
 		}
 
-		public Container AddScoped<TBase, TDerived>(bool lazy = false) where TDerived : TBase
+		public Container AddScoped<TBase, TDerived>() where TDerived : TBase
 		{
-			return AddScoped<TBase, TDerived>(lazy ? default : CreateAndInject<TDerived>());
-		}
-
-		public Container AddSingleton<TBase, TDerived>(TDerived? obj) where TDerived : TBase
-		{
-			Root.objects[typeof(TBase)] = new Registration(typeof(TDerived), obj);
+			registrations[typeof(TBase)] = new Registration(typeof(TDerived));
 			return this;
 		}
 
-		public Container AddScoped<TBase, TDerived>(TDerived? obj) where TDerived : TBase
+		public Container AddSingleton<TBase, TDerived>(TDerived obj) where TDerived : notnull, TBase
 		{
-			objects[typeof(TBase)] = new Registration(typeof(TDerived), obj);
+			return Root.AddScoped<TBase, TDerived>(obj);
+		}
+
+		public Container AddScoped<TBase, TDerived>(TDerived obj) where TDerived : notnull, TBase
+		{
+			objects[typeof(TDerived)] = obj;
+			registrations[typeof(TBase)] = new Registration(typeof(TDerived));
 			return this;
 		}
 
 		public Container AddTransient<TBase, TDerived>() where TDerived : TBase
 		{
-			objects[typeof(TBase)] = new Registration(typeof(TDerived), null, true);
+			registrations[typeof(TBase)] = new Registration(typeof(TDerived), true);
 			return this;
+		}
+
+		public void Instantiate()
+		{
+			foreach (Type item in registrations.Keys)
+			{
+				TryGet(item, out _);
+			}
+		}
+
+		private static bool TryGet(Container? container, Type type, out object? obj)
+		{
+			while (container != null)
+			{
+				if (container.TryGet_Internal(type, out obj))
+				{
+					return true;
+				}
+				container = container.Parent;
+			}
+			obj = null;
+			return false;
+		}
+
+		private bool TryGet_Internal(Type type, out object? obj)
+		{
+			if (registrations.TryGetValue(type, out Registration registration))
+			{
+				if (registration.Transient)
+				{
+					obj = CreateAndInject(registration.Type);
+				}
+				else if (!objects.TryGetValue(registration.Type, out obj))
+				{
+					obj = objects[registration.Type] = CreateAndInject(registration.Type);
+				}
+				return true;
+			}
+			obj = null;
+			return false;
 		}
 
 		private object CreateAndInject(Type type)
@@ -143,7 +168,5 @@ namespace Kryz.DI
 			reflectionInjector.Inject(type, obj, this);
 			return obj;
 		}
-
-		private T CreateAndInject<T>() => (T)CreateAndInject(typeof(T));
 	}
 }
