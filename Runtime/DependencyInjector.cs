22fbb175ded33ae74d2fb240d711612540ff7026
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2024-08-30 21:34:18 +0100

    Removed Unity references to make the package (hopefully) usable in any C# project. Unity Injector now does what was deleted here.

--- Runtime/DependencyInjector.cs
+++ Runtime/DependencyInjector.cs
@@ -1,20 +1,7 @@
-using UnityEngine;
-
 namespace Kryz.DI
 {
 	public static class DependencyInjector
 	{
 		public static readonly Container RootContainer = new();
-
-		static DependencyInjector()
-		{
-			Application.quitting += RootContainer.Clear;
-		}
-
-		[RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
-		private static void Init()
-		{
-			RootContainer.Clear();
-		}
 	}
 }
