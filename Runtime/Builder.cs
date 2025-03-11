687a7af9b16e7cdafac8e1d707924bbed1aca73a
Kryzarel <8752418+Kryzarel@users.noreply.github.com>
2025-03-11 12:21:53 +0000

    Changed DependencyGraph struct to DependencyValidator static class

--- Runtime/Builder.cs
+++ Runtime/Builder.cs
@@ -55,14 +55,14 @@ namespace Kryz.DI
 			ReadOnlyContainer container = parent != null ? new ReadOnlyContainer(parent, registrations, objects) : new ReadOnlyContainer(injector, registrations, objects);
 			RegisterInstance<IResolver>(container); // Register the Container itself as IResolver
 
-			DependencyGraph<Dictionary<Type, Registration>.KeyCollection> graph = new(container, injector, registrations.Keys);
-			if (graph.MissingDependencies != null && graph.MissingDependencies.Count > 0)
+			DependencyValidator.Data data = DependencyValidator.Validate(container, injector, registrations.Keys);
+			if (data.MissingDependencies != null && data.MissingDependencies.Count > 0)
 			{
-				throw new MissingDependencyException($"Can't build a container with missing dependencies: {FormatMissingDependencies(graph.MissingDependencies)}");
+				throw new MissingDependencyException($"Can't build a container with missing dependencies: {FormatMissingDependencies(data.MissingDependencies)}");
 			}
-			if (graph.CircularDependencies != null && graph.CircularDependencies.Count > 0)
+			if (data.CircularDependencies != null && data.CircularDependencies.Count > 0)
 			{
-				throw new CircularDependencyException($"Can't build a container with circular dependencies: {FormatCircularDependencies(graph.CircularDependencies)}");
+				throw new CircularDependencyException($"Can't build a container with circular dependencies: {FormatCircularDependencies(data.CircularDependencies)}");
 			}
 			return container;
 		}
---
+++ Runtime/DependencyValidator.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections.Generic;
+using Kryz.Collections;
+using Kryz.Utils;
+
+namespace Kryz.DI
+{
+	public static class DependencyValidator
+	{
+		public readonly struct Data
+		{
+			public readonly IReadOnlyDictionary<Type, IReadOnlyList<Type>>? MissingDependencies;
+			public readonly IReadOnlyDictionary<Type, IReadOnlyList<Type>>? CircularDependencies;
+
+			internal Data(IReadOnlyDictionary<Type, IReadOnlyList<Type>>? missing, IReadOnlyDictionary<Type, IReadOnlyList<Type>>? circular)
+			{
+				MissingDependencies = missing;
+				CircularDependencies = circular;
+			}
+		}
+
+		public static Data Validate<T>(IResolver resolver, IInjector injector, T types) where T : IEnumerable<Type>
+		{
+			NonAllocList<Type> visitedTypes = new();
+			Dictionary<Type, IReadOnlyList<Type>>? missing = null;
+			Dictionary<Type, IReadOnlyList<Type>>? circular = null;
+
+			foreach (Type type in types)
+			{
+				if (HasMissingDependency(type, resolver, injector, out IReadOnlyList<Type> missingTypes))
+				{
+					missing ??= new Dictionary<Type, IReadOnlyList<Type>>();
+					missing[type] = missingTypes;
+				}
+
+				if (HasCircularDependency(type, resolver, injector, ref visitedTypes))
+				{
+					circular ??= new Dictionary<Type, IReadOnlyList<Type>>();
+					circular[type] = visitedTypes.ToArray<Type, NonAllocList<Type>>();
+				}
+				visitedTypes.Clear();
+			}
+			visitedTypes.Dispose();
+
+			return new Data(missing, circular);
+		}
+
+		private static bool HasMissingDependency(Type type, IResolver resolver, IInjector injector, out IReadOnlyList<Type> missing)
+		{
+			List<Type>? missingTypes = null;
+
+			IReadOnlyList<Type> dependencies = injector.GetDependencies(type);
+			for (int i = 0; i < dependencies.Count; i++)
+			{
+				Type dependency = dependencies[i];
+				if (!resolver.TryGetType(type, out _))
+				{
+					missingTypes ??= new List<Type>();
+					missingTypes.Add(dependency);
+				}
+			}
+
+			missing = missingTypes != null ? missingTypes : Array.Empty<Type>();
+			return missing.Count > 0;
+		}
+
+		private static bool HasCircularDependency<TList>(Type type, IResolver resolver, IInjector injector, ref TList visitedTypes) where TList : IList<Type>
+		{
+			if (visitedTypes.Contains(type))
+			{
+				visitedTypes.Add(type);
+				return true;
+			}
+			visitedTypes.Add(type);
+
+			IReadOnlyList<Type> dependencies = injector.GetDependencies(type);
+			for (int i = 0; i < dependencies.Count; i++)
+			{
+				Type dependency = dependencies[i];
+				if (resolver.TryGetType(dependency, out Type? resolvedType))
+				{
+					if (HasCircularDependency(resolvedType, resolver, injector, ref visitedTypes))
+					{
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+	}
+}
